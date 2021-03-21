05d6a0f5d83b9ab409b7d5c9c8a637f714aba705
Proxypepe <some_email@ya.ru>
2021-03-21 19:12:01 +0300

    add docs

--- main.py
+++ main.py
@@ -3,22 +3,47 @@ import xlrd
 
 from topsis import Topsis
 from moora import Moora
+from electre import Electre
+
+
+def get_table(file):
+    start_matrix = []
+    tmp = []
+    weights = []
+    benefits = []
+    rb = xlrd.open_workbook(file, formatting_info=True)
+    sheet = rb.sheet_by_index(0)
+    tmp_sheet = [sheet.row_values(row_num) for row_num in range(sheet.nrows)]
+    m = len(tmp_sheet)
+    n = len(tmp_sheet[0])
+    for i in range(1, m - 3):
+        for j in range(1, n):
+            if tmp_sheet[i][j] != '':
+                tmp.append(tmp_sheet[i][j])
+        start_matrix.append(tmp.copy())
+        tmp.clear()
+    for j in range(1, n):
+        if tmp_sheet[m - 3][j] != '':
+            benefits.append(tmp_sheet[m - 3][j])
+
+    for j in range(1, n):
+        if tmp_sheet[m - 2][j] != '':
+            weights.append(tmp_sheet[m - 2][j])
+
+    return start_matrix, benefits, weights
 
 
 def main():
-    if sys.argv[2] == "--Topsis" or sys.argv[2] == "-t":
-        if ".xls" in sys.argv[1]:
-            rb = xlrd.open_workbook(sys.argv[1], formatting_info=True)
-            sheet = rb.sheet_by_index(0)
-            start_matrix = [sheet.row_values(row_num) for row_num in range(sheet.nrows)]
-            topsis = Topsis(start_matrix)
-            topsis.calc()
-    elif sys.argv[2].lower() == "--moora" or sys.argv[2] == "-m":
-        rb = xlrd.open_workbook(sys.argv[1], formatting_info=True)
-        sheet = rb.sheet_by_index(0)
-        start_matrix = [sheet.row_values(row_num) for row_num in range(sheet.nrows)]
-        moora = Moora(start_matrix)
-        moora.calc()
+    method = None
+    if ".xls" in sys.argv[1]:
+        if sys.argv[2] == "--Topsis" or sys.argv[2] == "-t":
+            method = Topsis(*get_table(sys.argv[1]))
+        elif sys.argv[2].lower() == "--Moora" or sys.argv[2] == "-m":
+            method = Moora(*get_table(sys.argv[1]))
+        elif sys.argv[2].lower() == "--Electre" or sys.argv[2] == "-e":
+            method = Electre(*get_table(sys.argv[1]))
+        if method is not None:
+            method.calc()
 
 
 if __name__ == '__main__':
